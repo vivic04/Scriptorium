d781886c0e69f64f155af910b8c08bc8b0d618a6
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2024-11-25 18:09:44 -0500

    moved files to designated folder

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Starting the application..."
-npm run dev
--- utils/logger.js
+++
@@ -1,5 +0,0 @@
-export const logError = (error) => {
-    if (process.env.NODE_ENV === 'development') {
-        console.error(error.toString());
-    }
-};
